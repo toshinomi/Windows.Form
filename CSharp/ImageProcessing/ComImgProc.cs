0930483be6d14903e3c2b16c39a3c4ff72dbbd59
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-06-26 21:44:39 +0900

    画像処理の基底クラス追加

---
+++ CSharp/ImageProcessing/ComImgProc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageProcessing
+{
+    abstract public class ComImgProc
+    {
+        protected Bitmap m_bitmap;
+
+        public ComImgProc(Bitmap _bitmap)
+        {
+            m_bitmap = _bitmap;
+        }
+
+        ~ComImgProc()
+        {
+            m_bitmap = null;
+        }
+
+        public Bitmap Bitmap
+        {
+            set { m_bitmap = value; }
+            get { return m_bitmap; }
+        }
+
+        abstract public bool GoImgProc(CancellationToken _token);
+    }
+}
