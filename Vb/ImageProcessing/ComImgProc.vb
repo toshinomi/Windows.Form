963dba6bc8366755a0e14c534e65f1d1522a1252
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-06-26 21:57:04 +0900

    画像処理の基底クラス追加

---
+++ Vb/ImageProcessing/ComImgProc.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Threading
+
+Namespace ImageProcessing
+    Public MustInherit Class ComImgProc
+        Protected m_bitmap As Bitmap
+
+        Public Sub New(ByVal _bitmap As Bitmap)
+            m_bitmap = _bitmap
+        End Sub
+
+        Public Property Bitmap() As Bitmap
+            Get
+                Return m_bitmap
+            End Get
+            Set(ByVal value As Bitmap)
+                m_bitmap = value
+            End Set
+        End Property
+
+        Public MustOverride Function GoImgProc(ByVal _token As CancellationToken) As Boolean
+    End Class
+End Namespace
