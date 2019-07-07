f8974590125f6987617bccff7868ea6bfd66f80e
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-07-07 18:38:32 +0900

    画像処理の設定画面追加

---
+++ CSharp/ImageProcessing/Lib/ComImageProcessingType.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing
+{
+    class ComImageProcessingType
+    {
+        private int m_Id;
+        private string m_Name;
+
+        public int Id
+        {
+            set { m_Id = value; }
+            get { return m_Id; }
+        }
+
+        public string Name
+        {
+            set { m_Name = value; }
+            get { return m_Name; }
+        }
+
+        public ComImageProcessingType()
+        {
+
+        }
+
+        public ComImageProcessingType(int _Id, string _Nmae)
+        {
+            m_Id = _Id;
+            m_Name = _Nmae;
+        }
+
+        ~ComImageProcessingType()
+        {
+        }
+
+        public object Clone()
+        {
+            return (ComImageProcessingType)MemberwiseClone();
+        }
+    }
+}
---
+++ CSharp/ImageProcessing/Views/FormSettingImageProcessing.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessing.Views
+{
+    public partial class FormSettingImageProcessing : Form
+    {
+        private List<ComImageProcessingType> m_items;
+
+        public FormSettingImageProcessing()
+        {
+            InitializeComponent();
+
+            LoadParam();
+        }
+
+        ~FormSettingImageProcessing()
+        {
+        }
+
+        public void LoadParam()
+        {
+            m_items = new List<ComImageProcessingType>();
+            m_items.Add(new ComImageProcessingType(Properties.Settings.Default.ImgTypeEdgeId, Properties.Settings.Default.ImgTypeEdgeName));
+            m_items.Add(new ComImageProcessingType(Properties.Settings.Default.ImgTypeGrayScaleId, Properties.Settings.Default.ImgTypeGrayScaleName));
+
+            cmbBoxImageProcessingType.Items.Add(m_items);
+            cmbBoxImageProcessingType.SelectedIndex = (int)m_items.Find(x => x.Name == Properties.Settings.Default.ImgTypeEdgeName)?.Id - 1;
+
+            return;
+        }
+
+        public void SaveParam()
+        {
+            ComImageProcessingType imgProcType = (ComImageProcessingType)cmbBoxImageProcessingType.SelectedItem;
+            Properties.Settings.Default.ImgTypeSelectName = imgProcType.Name;
+            Properties.Settings.Default.Save();
+
+            return;
+        }
+
+        private void OnClickOk(object sender, EventArgs e)
+        {
+            SaveParam();
+        }
+
+        private void OnClickCancel(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
