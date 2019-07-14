97a9fdb0653947a82ab25228d91e7b7248ce797a
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-07-14 15:41:52 +0900

    設定画面追加中

--- Cpp/ImageProcessing/FormSettingImageProcessing.cpp
+++ Cpp/ImageProcessing/FormSettingImageProcessing.cpp
@@ -1,6 +1,7 @@
 #include "FormSettingImageProcessing.h"
 
 using namespace ImageProcessing;
+using namespace System::Configuration;
 
 void FormSettingImageProcessing::SaveParam(void)
 {
@@ -10,4 +11,6 @@ void FormSettingImageProcessing::SaveParam(void)
 	//m_items.Add(new ComImageProcessingType(Properties.Settings.Default.ImgTypeBinarizationId, Properties.Settings.Default.ImgTypeBinarizationName));
 	//m_items.Add(new ComImageProcessingType(Properties.Settings.Default.ImgTypeGrayScale2DiffId, Properties.Settings.Default.ImgTypeGrayScale2DiffName));
 	//m_items.Add(new ComImageProcessingType(Properties.Settings.Default.ImgTypeColorReversalId, Properties.Settings.Default.ImgTypeColorReversalName));
+
+	String^ name = ::ConfigurationManager::AppSettings["ImgTypeEdgeId"];
 }
