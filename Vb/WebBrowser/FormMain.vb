82b4279ffe7c1f82e28c94ef60f66e2d7d22bf2f
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2019-12-08 22:38:06 +0900

    Update

--- Vb/WebBrowser/FormMain.vb
+++ Vb/WebBrowser/FormMain.vb
@@ -1,7 +1,13 @@
-﻿Public Class FormMain
+﻿''' <summary>
+''' MainFormのロジック
+''' </summary>
+Public Class FormMain
     Private m_nCountWebBrowser As UInt32
     Private m_listWebBrowser As Dictionary(Of UInt32, FormWebBrowser)
 
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
     Public Sub New()
 
         ' この呼び出しはデザイナーで必要です。
@@ -13,33 +19,66 @@
 
     End Sub
 
+    ''' <summary>
+    ''' デスクトラクタ
+    ''' </summary>
     Protected Overrides Sub Finalize()
         m_listWebBrowser.Clear()
         m_listWebBrowser = Nothing
         m_nCountWebBrowser = 0
     End Sub
 
+    ''' <summary>
+    ''' FormMainのロードイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnLoadFormMain(sender As Object, e As EventArgs) Handles MyBase.Load
         SetFormMain()
         SetFormChild()
     End Sub
 
+    ''' <summary>
+    ''' メニューの新規ウィンドウを開くのクリックイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnClickMenuFileNewOpen(sender As Object, e As EventArgs) Handles menuFile_NewOpen.Click
         SetFormChild()
     End Sub
 
+    ''' <summary>
+    ''' メニューの重ねて表示のクリックイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnClickMenuWindowCascade(sender As Object, e As EventArgs) Handles menuWindow_Cascade.Click
         Me.LayoutMdi(MdiLayout.Cascade)
     End Sub
 
+    ''' <summary>
+    ''' メニューの上下に並べて表示のクリックイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnClickMenuWindowTileHorizontal(sender As Object, e As EventArgs) Handles menuWindow_TileHorizontal.Click
         Me.LayoutMdi(MdiLayout.TileHorizontal)
     End Sub
 
+    ''' <summary>
+    ''' メニューの左右に並べて表示のクリックイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnClickMenuWindowTileVertical(sender As Object, e As EventArgs) Handles menuWindow_TileVertical.Click
         Me.LayoutMdi(MdiLayout.TileVertical)
     End Sub
 
+    ''' <summary>
+    ''' メニューの終了のクリックイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnClickMenuFileEnd(sender As Object, e As EventArgs) Handles menuFile_End.Click
         For Each nKey As UInt32 In m_listWebBrowser.Keys
             Dim Form As FormWebBrowser = m_listWebBrowser(nKey)
@@ -48,15 +87,26 @@
         Me.Close()
     End Sub
 
+    ''' <summary>
+    ''' Formのクローズイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">FormClosedイベントのデータ</param>
     Private Sub OnFormClosedFormMain(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         m_listWebBrowser.Clear()
         m_listWebBrowser = Nothing
     End Sub
 
+    ''' <summary>
+    ''' FormMainの設定
+    ''' </summary>
     Public Sub SetFormMain()
         Me.Size = New Size(1000, 600)
     End Sub
 
+    ''' <summary>
+    ''' FormMainの子ウィンドウの設定
+    ''' </summary>
     Public Sub SetFormChild()
         Dim form As New FormWebBrowser()
         form.MdiParent = Me
@@ -68,12 +118,20 @@
         m_nCountWebBrowser += 1
     End Sub
 
+    ''' <summary>
+    ''' Webブラウザの情報の取得
+    ''' </summary>
+    ''' <param name="nKey">キー</param>
+    ''' <returns>Webブラウザ情報</returns>
     Public Function GetFormWebBrowser(ByVal nKey As UInt32) As FormWebBrowser
         Return m_listWebBrowser(nKey)
     End Function
 
+    ''' <summary>
+    ''' Webブラウザの情報の削除
+    ''' </summary>
+    ''' <param name="nKey">キー</param>
     Public Sub RemoveListWebBrowser(ByVal nKey As UInt32)
         m_listWebBrowser.Remove(nKey)
     End Sub
-
 End Class
--- Vb/WebBrowser/FormWebBrowser.vb
+++ Vb/WebBrowser/FormWebBrowser.vb
@@ -1,8 +1,14 @@
-﻿Public Class FormWebBrowser
+﻿''' <summary>
+''' FormWebBrowserのロジック
+''' </summary>
+Public Class FormWebBrowser
     Private Const HOME_URL As String = "https://www.bing.com/"
     Private m_strUrl As String
     Private m_toolTip As ToolTip
 
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
     Public Sub New()
 
         ' この呼び出しはデザイナーで必要です。
@@ -13,35 +19,68 @@
         ' InitializeComponent() 呼び出しの後で初期化を追加します。
     End Sub
 
+    ''' <summary>
+    ''' デスクトラクタ
+    ''' </summary>
     Protected Overrides Sub Finalize()
         m_toolTip = Nothing
     End Sub
 
+    ''' <summary>
+    ''' Formのクローズイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">FormClosedイベントのデータ</param>
     Private Sub OnFormClosedFormWebBrowser(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
 
     End Sub
 
+    ''' <summary>
+    ''' FormWebBrowserのロードイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnLoadFormWebBrowser(sender As Object, e As EventArgs) Handles MyBase.Load
         SetInitToolTip()
         HomeWebBrowser()
     End Sub
 
+    ''' <summary>
+    ''' 前のページに戻すクリックイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnClickBack(sender As Object, e As EventArgs) Handles buttonBack.Click
         If (webBrowser.CanGoBack = True) Then
             webBrowser.GoBack()
         End If
     End Sub
 
+    ''' <summary>
+    ''' 次のページに移動クリックイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnClickForward(sender As Object, e As EventArgs) Handles buttonForward.Click
         If (webBrowser.CanGoForward = True) Then
             webBrowser.GoForward()
         End If
     End Sub
 
+    ''' <summary>
+    ''' 更新のクリックイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">イベントのデータ</param>
     Private Sub OnClickRefresh(sender As Object, e As EventArgs) Handles buttonRefresh.Click
         webBrowser.Refresh()
     End Sub
 
+    ''' <summary>
+    ''' URL入力のテキストボックスのキーダウンイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">キーイベントのデータ</param>
     Private Sub OnKeyDownTextUrl(sender As Object, e As KeyEventArgs) Handles textUrl.KeyDown
         If (e.KeyCode = Keys.Enter) Then
             m_strUrl = textUrl.Text
@@ -49,16 +88,27 @@
         End If
     End Sub
 
+    ''' <summary>
+    ''' WebBrowserのNavigatedイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">WebBrowserNavigatedイベントのデータ</param>
     Private Sub OnNavigatedWebBrowser(sender As Object, e As WebBrowserNavigatedEventArgs) Handles webBrowser.Navigated
         textUrl.Text = webBrowser.Url.AbsoluteUri
         Me.Text = webBrowser.Url.AbsoluteUri
     End Sub
 
+    ''' <summary>
+    ''' ツールチップの設定
+    ''' </summary>
     Public Sub SetInitToolTip()
         m_toolTip = New ToolTip()
         m_toolTip.SetToolTip(textUrl, "URL入力後、Enterキーを押してください!!!")
     End Sub
 
+    ''' <summary>
+    ''' ホームの処理
+    ''' </summary>
     Public Sub HomeWebBrowser()
         Dim uri As New Uri(HOME_URL)
         webBrowser.Navigate(uri)
